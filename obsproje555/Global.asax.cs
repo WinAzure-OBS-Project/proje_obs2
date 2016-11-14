a8e34f27c51c6d0aa9b6e6a452934ceb8192bb4a
denizcetiner <deniz.cetiner94@gmail.com>
2016-11-14 19:33:24 +0200

    Global.asax.cs dosyası giriş işlemleri için edit

--- obsproje555/Global.asax.cs
+++ obsproje555/Global.asax.cs
@@ -1,21 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
-namespace obsproje555
+namespace proje_obs
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                //Extract the forms authentication cookie
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+
+                // If caching roles in userData field then extract
+                string[] roles = authTicket.UserData.Split(new char[] { '|' });
+                // Create the IIdentity instance
+                IIdentity id = new FormsIdentity(authTicket);
+
+                // Create the IPrinciple instance
+                IPrincipal principal = new GenericPrincipal(id, roles);
+
+                // Set the context user 
+                Context.User = principal;
+            }
         }
     }
 }
