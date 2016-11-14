f7fb84d1172f066f6c73182b379c44a36910a17b
Deniz <Deniz@DESKTOP-IF0G3V9>
2016-11-14 16:00:30 +0300

    HomeController eklendi

--- obsproje555/Controllers/HomeController.cs
+++ obsproje555/Controllers/HomeController.cs
@@ -4,26 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace obsproje555.Controllers
+namespace proje_obs.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
-        }
+            if (Session["Role"] != null)
+            {
+                String role = Session["Role"].ToString();
+                if (role == "Ogrenci")
+                {
+                    return RedirectToAction("Index", "Ogrenci");
+                }
+                else if (role == "OgretimElemani")
+                {
+                    return RedirectToAction("Index", "OgretimElemani");
+                }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            }
             return View();
         }
     }
