7631d236240b74682dfcc53e13b38e9d2ad21c53
Deniz <Deniz@DESKTOP-IF0G3V9>
2016-11-14 15:57:08 +0300

    Add project files.

---
+++ obsproje555/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(obsproje555.Startup))]
+namespace obsproje555
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
