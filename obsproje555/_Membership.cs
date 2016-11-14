aab7eec9c74a2105e9786484a685053d71500241
burakteke <burakteke37@gmail.com>
2016-11-14 19:01:40 +0300

    _Membership.cs
    Membership kodları eklendi

---
+++ obsproje555/_Membership.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace proje_obs
+{
+    public static class _Membership
+    {
+        public static void Login(String username, String role, HttpResponseBase Response)
+        {
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(
+                        1,
+                        username,
+                        DateTime.Now,
+                        DateTime.Now.AddMinutes(30),
+                        false,
+                        role,
+                        FormsAuthentication.FormsCookiePath);
+
+            string EncryptedTicket = FormsAuthentication.Encrypt(ticket);
+
+            HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, EncryptedTicket);
+            cookie.HttpOnly = true;
+            Response.Cookies.Add(cookie);
+        }
+
+        public static void Logout()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
