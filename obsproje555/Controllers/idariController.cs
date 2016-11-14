9fb518007d6ffa5f814d64b00eca6b53cb5717ef
melikeyurtoglu <aysemelikeyurtoglu@gmail.com>
2016-11-14 20:33:10 +0200

    Idari controller eklendi.
    Signed-off-by: melikeyurtoglu <aysemelikeyurtoglu@gmail.com>

---
+++ obsproje555/Controllers/idariController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proje_obs.Controllers
+{
+    public class idariController : Controller
+    {
+        String authorize = "idari";
+        // GET: idari
+        public ActionResult Index()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("Login");
+            }
+            else if (!User.IsInRole(authorize))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var ctx = new ObsDbContext();
+            int Id = Convert.ToInt32(User.Identity.Name);
+            var o = ctx.Idari.FirstOrDefault(a => a.Id == Id);
+            if(o!=null)
+            {
+                return View(o);
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(int Id, String password)
+        {
+            ObsDbContext ctx = new ObsDbContext();
+            var ogr = ctx.Idari.FirstOrDefault(o => o.Id == Id);
+            if (ogr != null)
+            {
+                _Membership.Login(Convert.ToString(Id), "idari", Response);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            _Membership.Logout();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult UploadBanner()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadBanner(HttpPostedFileBase file)
+        {
+            _Storage.UploadFile(file, "__banner.jpg");
+            return RedirectToAction("Index");
+        }
+    }
+}
